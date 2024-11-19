6e8a10ebb01409c0fa3870db79cd4e84382ae3c7
Jordan <jordan.valente@hotmail.fr>
2024-11-19 08:48:48 +0100

    ajout du type

--- src/Pages/PokemonDetailPage.jsx
+++ src/Pages/PokemonDetailPage.jsx
@@ -20,6 +20,7 @@ const PokemonDetailPage = () => {
 
             const res = await PokemonsServices.getPokemonInfo(name);
             setPokemon({ ...response.data, ...res.data });
+            console.log(res.data.types);
             console.log(res.data.game_indices);
 
 
@@ -35,17 +36,18 @@ const PokemonDetailPage = () => {
 
 
 
-    return <Container className="d-flex flex-row align-items-center" >
+    return <Container className="d-flex flex-row align-items-start gap-4 p-4" >
 
         <div style={{ backgroundColor: "grey" }}>
             <img style={{ width: "16rem" }} src={"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/" + pokemon.id + ".png"} alt="illustration pokemon" />
         </div>
-        <div>
+        <div className="d-flex flex-column justify-content-center flex-wrap gap-2">
             {pokemon.game_indices && pokemon.game_indices.map((game) => {
                 return <Button >{game.version.name} </Button>
             })}
+
         </div>
-        <div >
+        <div className="d-flex flex-column justify-content-start flex-wrap gap-2">
             <h1 >{pokemon.name && pokemon.names[4].name}</h1>
 
             <p>Numéro Pokédex: {pokemon.order}</p>
@@ -54,6 +56,12 @@ const PokemonDetailPage = () => {
             <p>Lieu de vie : {pokemon.habitat && pokemon.habitat.name}</p>
             <p>Versions : {pokemon.habitat && pokemon.habitat.name}</p>
             <Button variant="primary" onClick={() => { navigateTo("/") }}>Retour aux Pokemon</Button>
+        </div>
+
+        <div>
+            {pokemon.types && pokemon.types.map((type) => {
+                return <Button>{type.type.name}</Button>
+            })}
         </div>
 
 
